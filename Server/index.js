613f3e5172cce7e376bff688c2a01f96fb3b0ce7
Po-Yu Liu <alan.pyliu@gmail.com>
2020-10-21 20:47:11 -0500

    updates 1

--- Server/index.js
+++ Server/index.js
@@ -1,23 +1,19 @@
 // index.js
 const http = require('http');
+const serviceNow = require('./serviceNow')
 
 // declare server variables
-const hostname = '127.0.0.1';
-const port = 8080;
+//const hostname = '127.0.0.1';
+//const port = 8080;
 
 const server = http.createServer((req, res) => {
   req.on('data', (data) => {
-    console.log("Received a notification")
-    console.log(data)
+    console.log("Received a notification");
+    console.log("test");
+    console.log(data);
   })
 
   res.statusCode = 200;
   res.setHeader('Content-Type', 'text/plain');
   res.end('Hello World\n');
-});
-
-server.listen(port, hostname, () => {
-  console.log(`Server is running at http://${hostname}:${port}/`);
-});
-
-//https://tasty-eel-78.loca.lt:3000
+}).listen(8080); 
---
+++ Server/serviceNow.js
@@ -0,0 +1,27 @@
+const axios = require('axios')
+const url = 'https://dev63377.service-now.com/api/now/table/incident?sysparm_display_value=true&sysparm_exclude_reference_link=true&sysparm_fields=number%2Cshort_description'
+
+const user = 'admin'
+const pwd = 'Winter2020@#'
+
+function pullIncidentTable () {
+    axios.post(url,
+        {
+            headers: {
+                'Content-Type': 'application/json',
+                'Accept': 'application/json',
+                'Authorization': 'Basic ' + Buffer.from(user + ':' + pwd).toString('base64'),
+            }
+        }).then((response) => {
+            console.log(response)
+        })
+        .catch((error) => {
+            console.log("==================== ERROR ====================", error);
+        })
+}
+
+function createIncidentTicket () {
+
+}
+
+module.exports = {pullIncidentTable, createIncidentTicket}
