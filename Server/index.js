1d580c51b2a326511a86db541eed940435ff5447
Alan Liu <alan.pyliu@gmail.com>
2020-10-22 22:35:28 -0500

    it works!!!!

--- Server/index.js
+++ Server/index.js
@@ -1,21 +1,44 @@
 const express = require('express')
 const bodyParser = require('body-parser')
 const request = require('request')
+const serviceNow = require('./serviceNow')
 const app = express()
 const port = 8080
 
 app.use(bodyParser.urlencoded({ extended: false}))
 
-app.post('/', (req, res) => {
-  console.log('Received post request')
-  var data='';
+app.get('/', (req, res) => {
+  console.log('Received get request')
+  
+  let data='';
   req.setEncoding('utf8');
   req.on('data', function(chunk) { 
      data += chunk;
   });
 
   req.on('end', function() {
-    response = JSON.parse(data)
+    let response = JSON.parse(data)
+    console.log(response)
+  });
+})
+
+app.post('/', (req, res) => {
+  console.log('Received post request')
+  
+  let data='';
+  req.setEncoding('utf8');
+  req.on('data', function(chunk) { 
+     data += chunk;
+  });
+
+  req.on('end', function() {
+    let response = JSON.parse(data)
+    console.log(response)
+
+    if(response.NewStateValue === 'ALARM') {
+      console.log('creating ticket')
+      serviceNow.createIncidentTicket()
+    }
     if(response.Type === 'SubscriptionConfirmation') {
       console.log(response.SubscribeURL)
       request(response.SubscribeURL, (error, response, body) => {
